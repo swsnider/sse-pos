68f0528b8c53e59d7672315e3a4ca77cd2d3e24d
Silas Snider <ssnider@silas-sniders-mac-mini.local>
2009-06-06 14:07:22 -0400

    fixed percentage issue

--- models/__init__.py
+++ models/__init__.py
@@ -41,7 +41,7 @@ class LineItem(db.Model):
         else:
             return self.color.discount
     def total(self):
-        return (self.category.price*int(self.quantity)*((100 - self.get_dicount())/100.0)) + self.misc_amount
+        return (self.category.price*int(self.quantity)*((100 - self.get_discount())/100.0)) + self.misc_amount
     def total_str(self):
         return "%#.2f" % self.total()
 
