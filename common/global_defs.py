1d754af1caa04406e9e4086792220d90aa7c3a6a
Silas Snider <swsnider@gmail.com>
2011-04-23 17:34:20 -0400

    css

--- common/global_defs.py
+++ common/global_defs.py
@@ -3,22 +3,20 @@ DEBUG = ('SERVER_SOFTWARE' not in os.environ) or (os.environ['SERVER_SOFTWARE'].
 APPS = None
 if DEBUG:
   APPS = dict(
-    default = 'http://localhost:8080',
+    pos = 'http://localhost:8080',
     static = 'http://localhost:8081',
     donations = 'http://localhost:8082',
     inventory = 'http://localhost:8083',
     mapreducer = 'http://localhost:8084',
-    pos = 'http://localhost:8085',
-    admin = 'http://localhost:8086',
+    admin = 'http://localhost:8085',
   )
 else:
   APPS = dict(
-    default = 'http://sse-pos.appspot.com',
+    pos = 'http://pos.sse-pos.appspot.com',
     static = 'http://static.sse-pos.appspot.com',
     donations = 'http://donations.sse-pos.appspot.com',
     inventory = 'http://inventory.sse-pos.appspot.com',
     mapreducer = 'http://mapreducer.sse-pos.appspot.com',
-    pos = 'http://pos.sse-pos.appspot.com',
     admin = 'http://admin.sse-pos.appspot.com',
   )
 SESSION_OPTS = {
