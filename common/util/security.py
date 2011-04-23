48174637b83aa25d89b50492fc06c20947589687
Silas Snider <swsnider@gmail.com>
2011-04-23 17:35:12 -0400

    reducing to the java source

--- common/util/security.py
+++
@@ -1,8 +0,0 @@
-import bottle
-
-def secure(f):
-  def g(*args, **kwargs):
-    session = bottle.request.environ.get('beaker.session')
-    if 'current_user' not in session:
-      bottle.redirect('/login')
-    return f(*args, **kwargs)
