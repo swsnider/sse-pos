9b09c20b3f0bacfc504875b47098006b032500e4
Silas Snider <swsnider@gmail.com>
2011-04-24 10:22:37 -0400

    adding check for haml gem to the build script

--- scripts/BUILD.sh
+++ scripts/BUILD.sh
@@ -6,6 +6,12 @@ then
   exit;
 fi
 
+if [ 0 -ne `gem list --local | grep "haml" | wc -l`]
+then
+  echo "Installing gem for sass";
+  sudo gem install haml || (echo "Install failed -- exiting..." && exit);
+fi
+
 if [ -d build_out ]
 then
   if [ -d "build_out/$1" ]
